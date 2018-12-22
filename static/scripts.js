41ada1a8d8f25836d76e198d4641fe0e465fda3a
Mark Bebawy <markbebawy0@gmail.com>
2018-12-22 17:46:30 +0100

    Added cross function for crossing out names

---
+++ static/scripts.js
@@ -0,0 +1,20 @@
+function validateInput() {
+    var name, groupName, text;
+
+    /* Get the value of the input field with id="name" */
+    name = document.getElementById("name").value;
+    groupName = document.getElementById("groupName").value;
+
+    /*  */
+    if (name == "" || groupName == "")
+        text = "Name or groupname cannot be empty!";
+    else
+        text = "Hello " + name + "! Your group is: " + groupName;
+    document.getElementById("nameView").innerHTML = text;
+}
+
+function cross(name) {
+    var button = document.getElementById("button_{{name}}");
+    
+    button.firstChild.data = "Poffertjes";
+}
