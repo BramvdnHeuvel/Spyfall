13cb8909363afe4b4b760ed0cbbe3e0d90694ca9
Schnipeltaper <sjmritchie@gmail.com>
2018-12-22 18:20:38 +0100

    Hoofpagina en tweede pagina

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def functie():
-    return render_template('main.html')
+    return render_template('spyfall.html',names=['sam','mark'],locations=['strand', 'duinen'])
 
 app.run(debug = True)
 
