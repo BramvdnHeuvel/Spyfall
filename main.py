1428c123550979c49c1b9c23bb7623acdb4032db
Bram <bram.vdh1999@gmail.com>
2018-12-23 16:50:09 +0100

    Add API route to discover role

--- main.py
+++ main.py
@@ -60,12 +60,23 @@ def join_group(group, name):
     #   players     - (List of player objects)
     # }
 
+@app.route('/api/v1/<group>/myrole/<name>')
+def discover_role(group, name):
+    pass # TODO: Receive a one-time object with the player's role and location
+    # The client expects a dictionary with at least the following properties:
+    # {
+    #   location    - (String. Unknown if the player is the spy)
+    #   role        - (String. Equal to "Spy" if the player is the spy)
+    # }
+    # If the player's role has already been discovered, send an error value.
+
 @app.route('/game/<name>')
 def show_game(name):
     global visitors
     visitors.append(name)
     stream.send_msg("USER UPDATE", 'yay')
     return render_template('index.html', name=name)
+
 
 app.run(debug = True)
 
