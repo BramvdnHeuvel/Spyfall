ab379b9be0f800ade429ec9fd0d71b10af913a4e
Bram <bram.vdh1999@gmail.com>
2018-12-24 02:32:52 +0100

    Refactor: JS Implementation

--- static/viewer.js
+++ static/viewer.js
@@ -13,14 +13,15 @@ var viewer = new Vue({
     methods: {
         updateUsers: function() {
             var self = this;
-            getData('/api/v1/' + this.group + '/players', function(data) {
+            getData('/api/v1/' + self.group + '/players', function(data) {
                 self.players = data.map((user) => ({name: user}));
             });
         },
 
         leaveGroup: function(event) {
             console.log("Leaving group!");
-            getData('/api/v1/' + this.group + '/leave/' + this.myName, function(data) {
+            var self = this;
+            getData('/api/v1/' + self.group + '/leave/' + self.myName, function(data) {
                 // Leave empty, 'cause nothing useful is returned.
             })
             this.group = undefined;
@@ -28,29 +29,12 @@ var viewer = new Vue({
         },
 
         joinGroup: function(event){
-            frame="gameMenu";
+            this.frame="gameMenu";
             var self = this;
             getData('/api/v1/' + self.group + '/join/' + self.myName, function(data) {
                 console.log(data);
                 self.players = data.players;
             });
-
-        }
-        // Used to toggle style on clicked user lists
-        clickUser: function(event) {
-            var button = event.target;
-            var user = this.players.find(x => x.name === button.innerText);
-
-            while (button.type != 'submit') {
-                button = button.parentElement;
-            }
-
-            user.crossed = !user.crossed;
-            if (user.crossed) {
-                button.style.backgroundColor = "rgba(255, 0, 0, 0.3)";
-            } else {
-                button.style.backgroundColor = "green";
-            }
         },
 
         createGame: function(event) {
