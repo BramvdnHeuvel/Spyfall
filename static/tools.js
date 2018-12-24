441538de802ebddcd2b877dcb8ffc8a10eee3fcc
Bram <bram.vdh1999@gmail.com>
2018-12-24 02:31:00 +0100

    Refactor: Stream SSE

--- static/tools.js
+++ static/tools.js
@@ -2,3 +2,14 @@ function getData(link, callback) {
     fetch(link).then((response) => (response.json()))
         .then((data) => {callback(data)});
 }
+
+function listentoStream(group, callback) {
+    var source = new EventSource('/stream/' + group);
+    source.onmessage = function(event) {
+        console.log("Detected stream msg: " + event.data);
+        callback(event.data);
+    }
+
+    console.log("EventSource initiated for group " + group + ".");
+    return source;
+}
