f91e6087b3165c8b4df03adabccaca297de6f31e
Bram <bram.vdh1999@gmail.com>
2018-12-27 09:27:51 +0100

    Add random name generator

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, Response, jsonify
-from res import stream
+from res import stream, names
 import res.data as data
 import os
 import sys
@@ -12,7 +12,7 @@ def stream_data(channel):
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('index.html', name=names.random_name())
 
 
 # -----------------------------------------------------
@@ -73,4 +73,4 @@ if __name__ == "__main__":
         port = int(os.environ.get('PORT', 5000))
         app.run(host='0.0.0.0', port=port)
     else:
-        app.run()
+        app.run(debug=True)
