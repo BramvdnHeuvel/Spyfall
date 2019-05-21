e493b57b9ecfe17f769fc8e0555b05a546362b25
Bram <bram.vdh1999@gmail.com>
2019-05-22 00:20:09 +0200

    Final design update

--- res/keuzemenu.py
+++ res/keuzemenu.py
@@ -7,7 +7,7 @@ events = [
         'no': []
     },
     {
-        'name': 'Spelletjes spellen',
+        'name': 'Spelletjes spelen',
         'desc': 'Lekker met zijn allen een avondje spelletjes spelen bij iemand thuis!',
         'yes': ['Brom', 'Swammy', 'Riineer'],
         'maybe': ["Harissa", "Seneca", "Egdar", "Mickey", "Meesje"],
@@ -19,6 +19,13 @@ events = [
         'yes': ['Brom', 'Mork', 'Riineer'],
         'maybe': ["Harissa", "Seneca", "Egdar", "Mickey", "Meesje"],
         'no': ['Swammy']
+    },
+    {
+        'name': 'Een cafeetje pakken',
+        'desc': 'Een drankje doen met je matties in een heerlijk café.',
+        'yes': ['Brom', 'Swammy', 'Riineer'],
+        'maybe': ["Harissa", "Seneca", "Egdar", "Mickey", "Meesje"],
+        'no': ['Mork']
     },
     {
         'name': 'Escape room',
@@ -35,13 +42,6 @@ events = [
         'no': []
     },
     {
-        'name': 'Een cafeetje pakken',
-        'desc': 'Een drankje doen met je matties in een heerlijk café.',
-        'yes': ['Swammy', 'Riineer'],
-        'maybe': ['Brom', "Harissa", "Seneca", "Egdar", "Mickey", "Meesje"],
-        'no': ['Mork']
-    },
-    {
         'name': 'Hackathon',
         'desc': 'De Spyfall website waar we ons NU op bevinden - is best wel een troep. Het idee om terug te gaan, nog een poging te wagen en menig hart te verblijden, zal iedereen gelukkig maken.',
         'yes': ['Brom', 'Swammy'],
@@ -56,6 +56,13 @@ events = [
         'no': ['Swammy']
     },
     {
+        'name': 'Bram helpen klussen',
+        'desc': 'Bram heeft een nieuwe kamer... wie zou hem nou niet willen ondersteunen in het opknappen van zijn kamer?',
+        'yes': ["Riineer", 'Brom'],
+        'maybe': ["Harissa", "Mork", "Seneca", "Egdar", "Mickey", "Meesje"],
+        'no': ['Swammy']
+    },
+    {
         'name': 'D&D Oneshot',
         'desc': 'Voor de D&D liefhebbers is er niets leukers dan te proberen een klein avontuur te beleven met hun favoriete X-Files vrienden!',
         'yes': ['Brom'],
@@ -63,10 +70,10 @@ events = [
         'no': []
     },
     {
-        'name': 'Bram helpen klussen',
-        'desc': 'Bram heeft een nieuwe kamer... wie zou hem nou niet willen ondersteunen in het opknappen van zijn kamer?',
+        'name': 'Picknick & zwemmen',
+        'desc': 'Zeg nou zelf, wat is er leuker op een warme zomerdag dan gezellig picknicken en af en toe zwemmen op een prachtige locatie?',
         'yes': ['Brom'],
-        'maybe': ["Harissa", "Mork", "Seneca", "Egdar", "Mickey", "Riineer", "Meesje"],
-        'no': ['Swammy']
+        'maybe': ["Harissa", "Mork", "Seneca", "Swammy", "Egdar", "Mickey", "Riineer", "Meesje"],
+        'no': []
     }
 ]
