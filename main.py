7f7f39f92870c13e0b42f73950095b72d3b5945e
Bram <bram.vdh1999@gmail.com>
2019-05-21 13:14:16 +0200

    Counter #2

--- main.py
+++ main.py
@@ -135,6 +135,7 @@ def evil_sister_image():
 @app.route('/counter/reset')
 def create_database():
     keuzes.init_database()
+    return 'OK! Database reset!'
 
 @app.route('/counter/add')
 def get_counter():
