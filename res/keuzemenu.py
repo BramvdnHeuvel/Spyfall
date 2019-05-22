2d62f27aa9e1d7370d71371cfd61856b2e387b56
Bram <bram.vdh1999@gmail.com>
2019-05-22 17:21:33 +0200

    Update keuzemenu

--- res/keuzemenu.py
+++ res/keuzemenu.py
@@ -2,44 +2,65 @@ events = [
     {
         'name': 'Lasergamen',
         'desc': 'In Amersfoort een uurtje lasergamen met max. 13 personen... wie wil dat nou niet?',
-        'yes': ['Brom', 'Mork', 'Swammy', 'Egdar'],
-        'maybe': ["Harissa", "Seneca", "Mickey", "Riineer", "Meesje"],
+        'yes': ['Brom', 'Mork', 'Swammy', 'Egdar', 'Seneca'],
+        'maybe': ["Harissa", "Mickey", "Riineer", "Meesje"],
         'no': []
-    },
-    {
-        'name': 'Spelletjes spelen',
-        'desc': 'Lekker met zijn allen een avondje spelletjes spelen bij iemand thuis!',
-        'yes': ['Brom', 'Swammy', 'Riineer', 'Egdar'],
-        'maybe': ["Harissa", "Seneca", "Mickey", "Meesje"],
-        'no': ['Mork']
     },
     {
         'name': 'Een cafeetje pakken',
         'desc': 'Een drankje doen met je matties in een heerlijk café.',
-        'yes': ['Brom', 'Swammy', 'Egdar', 'Riineer'],
-        'maybe': ["Harissa", "Seneca", "Mickey", "Meesje"],
+        'yes': ['Brom', 'Swammy', 'Egdar', 'Riineer', 'Seneca'],
+        'maybe': ["Harissa", "Mickey", "Meesje"],
         'no': ['Mork']
     },
     {
-        'name': 'Niks doen',
-        'desc': 'Soms kan het zo simpel zijn.',
-        'yes': ['Brom', 'Mork', 'Riineer'],
-        'maybe': ["Harissa", "Seneca", "Egdar", "Mickey", "Meesje"],
-        'no': ['Swammy']
+        'name': 'Spelletjes spelen',
+        'desc': 'Lekker met zijn allen een avondje spelletjes spelen bij iemand thuis!',
+        'yes': ['Brom', 'Swammy', 'Riineer', 'Egdar', 'Seneca'],
+        'maybe': ["Harissa", "Mickey", "Meesje"],
+        'no': ['Mork']
     },
     {
         'name': 'Sietse helpen daten',
         'desc': 'Sietse is alleen, eenzaam en single. Wie helpt hem aan een vriendin? en Bram is Gay.',
-        'yes': ['Brom', 'Egdar', 'Riineer'],
-        'maybe': ["Harissa", "Mork", "Seneca", "Mickey", "Meesje"],
+        'yes': ['Brom', 'Egdar', 'Riineer', 'Seneca'],
+        'maybe': ["Harissa", "Mork", "Mickey", "Meesje"],
         'no': ['Swammy']
     },
     {
         'name': 'Escape room',
         'desc': 'Wat is er nou leuker dan een escape room doen? Het staat al zo lang in ons vizier, tijd om het nu ECHT te doen!',
-        'yes': ['Brom', 'Swammy'],
+        'yes': ['Brom', 'Swammy', 'Egdar'],
         'maybe': ["Harissa", "Mork", "Seneca", "Mickey", "Riineer", "Meesje"],
-        'no': ['Egdar']
+        'no': []
+    },
+    {
+        'name': 'Bram helpen klussen',
+        'desc': 'Bram heeft een nieuwe kamer... wie zou hem nou niet willen ondersteunen in het opknappen van zijn kamer?',
+        'yes': ["Riineer", 'Brom', 'Seneca'],
+        'maybe': ["Harissa", "Mork", "Egdar", "Mickey", "Meesje"],
+        'no': ['Swammy']
+    },
+    {
+        'name': 'Niks doen',
+        'desc': 'Soms kan het zo simpel zijn.',
+        'yes': ['Brom', 'Mork', 'Riineer'],
+        'maybe': ["Harissa", "Egdar", "Mickey", "Meesje"],
+        'no': ['Swammy', 'Seneca']
+    },
+    {
+        'name': 'Picknick en zwemmen',
+        'desc': 'Zeg nou zelf, wat is er leuker op een warme zomerdag dan gezellig picknicken en af en toe zwemmen op een prachtige locatie?',
+        'yes': ['Brom', 'Riineer'],
+        'maybe': ["Harissa", "Mork", "Seneca", "Swammy", "Egdar", "Mickey", "Meesje"],
+        'no': []
+    },
+    {
+        'name': 'LAN Party',
+        'desc': 'De mensen die hieraan meegedaan hebben, kunnen in geuren en kleuren vertellen hoe leuk het is; spelletjes spelen, maar dan digitaal! Niets is leuker dan je kapot lachen bij het spelen van debiele spelletjes, het competitief spelen van een debiel spelletje of het proberen samen te overleven in een Minecraft wereld.',
+        'yes': ['Brom', 'Seneca'],
+        'maybe': ["Harissa", "Mork", 'Riineer', "Swammy", "Egdar", "Mickey", "Meesje"],
+        'no': []
     },
     {
         'name': 'Poulen',
@@ -49,31 +70,17 @@ events = [
         'no': ['Egdar']
     },
     {
-        'name': 'Bram helpen klussen',
-        'desc': 'Bram heeft een nieuwe kamer... wie zou hem nou niet willen ondersteunen in het opknappen van zijn kamer?',
-        'yes': ["Riineer", 'Brom'],
-        'maybe': ["Harissa", "Mork", "Seneca", "Egdar", "Mickey", "Meesje"],
-        'no': ['Swammy']
-    },
-    {
         'name': 'Hackathon',
         'desc': 'De Spyfall website waar we ons NU op bevinden - is best wel een troep. Het idee om terug te gaan, nog een poging te wagen en menig hart te verblijden, zal iedereen gelukkig maken.',
         'yes': ['Brom', 'Swammy'],
-        'maybe': ["Harissa", "Mork", "Seneca", "Mickey", "Meesje"],
-        'no': ['Riineer', 'Egdar']
+        'maybe': ["Harissa", "Mork", "Mickey", "Meesje"],
+        'no': ['Riineer', 'Egdar', 'Seneca']
     },
     {
         'name': 'DnD Oneshot',
         'desc': 'Voor de D&D liefhebbers is er niets leukers dan te proberen een klein avontuur te beleven met hun favoriete X-Files vrienden!',
         'yes': ['Brom'],
-        'maybe': ["Harissa", "Mork", "Swammy", "Seneca", "Egdar", "Mickey", "Riineer", "Meesje"],
-        'no': []
-    },
-    {
-        'name': 'Picknick en zwemmen',
-        'desc': 'Zeg nou zelf, wat is er leuker op een warme zomerdag dan gezellig picknicken en af en toe zwemmen op een prachtige locatie?',
-        'yes': ['Brom'],
-        'maybe': ["Harissa", "Mork", "Seneca", "Swammy", "Egdar", "Mickey", "Riineer", "Meesje"],
-        'no': []
+        'maybe': ["Harissa", "Mork", "Swammy", "Egdar", "Mickey", "Riineer", "Meesje"],
+        'no': ['Seneca']
     }
 ]
